db03d1743f604a8d6e41653f185030aa089f3495
Shrajen <bimeshrajbanshi1999@gmail.com>
2024-03-12 19:26:45 +0545

    use of contains and change global time out

--- cypress/e2e/my _first_test.cy.js
+++ cypress/e2e/my _first_test.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 it('google test', ()=>{
-    cy.visit("https://google.com")
-    cy.get('#APjFqb').type('bimesh rajbanshi linkedin {enter}')
+    cy.visit("https://google.com");
+    cy.get('#APjFqb').type('bimesh rajbanshi linkedin {enter}');
+    cy.contains('Change to English').click();
 })
